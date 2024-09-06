ea2ad41440a93da8454821f1b9efe80c1f92530f
UyenPun <tranuyen2018@gmail.com>
2024-09-06 10:18:40 +0700

    Arrow Function.

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
 
   return (
     <>
-      {console.log("Component rendered")} {/* Để kiểm tra khi component render */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

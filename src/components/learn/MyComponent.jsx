3125a3605beb2cc937e2dc6e6fed238a19b69f1f
UyenPun <tranuyen2018@gmail.com>
2024-09-07 11:50:37 +0700

    30. JSX.

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import './style.css'
+//  Fragment
 const MyComponent = () => {
   return (
-    <div>HelloOOOOOOOOOOOOOOOOOOOOO</div>
+    <>
+      <div>HelloOOOOOOOOOOOOOOOOOOOOO</div>
+      <div className='child'
+      style={{borderRadius: '10px'}}
+      >Aloo</div>
+    </>
   );
 }
 

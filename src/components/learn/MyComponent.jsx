bd8064d8f783633c9d345a34a4a673a51f140060
UyenPun <tranuyen2018@gmail.com>
2024-09-08 17:09:20 +0700

    Cach su dung bien so voi JSX.

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './style.css'
+
 //  Fragment
 const MyComponent = () => {
+  const name = 'Uyen';
+  // const number = 1;
+  const numbers = [1, 2, 3, 4, 5];
+  const infor = {
+    name: "Uyen",
+    age: 25
+  }
+
+  // SON.stringify -> convert sang string
   return (
     <>
-      <div>HelloOOOOOOOOOOOOOOOOOOOOO</div>
+      <div>HelloOOOOOOOOOOOOOOOOOOOOO! {JSON.stringify(name)}</div>
+      <div>HelloOOOOOOOOOOOOOOOOOOOOO! {JSON.stringify(infor)}</div>
+      <div>HelloOOOOOOOOOOOOOOOOOOOOO! {JSON.stringify(numbers)}</div>
+
+      <div>{console.log("Morning!")}</div>
+
       <div className='child'
       style={{borderRadius: '10px'}}
       >Aloo</div>
